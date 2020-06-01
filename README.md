4e00e0a671534c1852741ca6969615bd4b00ccc9
anil <anil@mangodata.co>
2020-06-02 00:35:02 +0530

    django myresume chatbot

---
+++ README.md
@@ -0,0 +1,39 @@
+# Django & jQuery MyResume-BOT Chat Application
+A simple MyResume chatbot build with Django, Jquery & chatbotAI.
+
+## Installation
+
+Installation is simplest:
+
+1. Clone the repository
+2. Install dependencies with `pip install -r requirements.txt`
+
+#### Installation notes
+
+MyResume-BOT depends on:
+ 1. `Django`
+ 2. `chatbotAI`
+
+## What He Can Tell You.
+
+- You can ask him about anything related to my resume.
+- You can ask him about software development tools and terminologies.
+- You can ask him to show recent questions posted on stackoverflow for specific tag
+- You can ask him to display recent answered questions posted on stackoverflow for specific tag
+- Specify the no. of questions or he will get 5 results.
+- He provide you Chat Summary too when the chat ends.
+
+## Demo link
+
+
+```
+python bot.py
+```
+
+output is:
+
+![output](output.PNG)
+
+## Author
+
+* **Anil Kumar Prajapati** 
---
+++ static/chat.js
@@ -0,0 +1,84 @@
+$('#chat-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+        url : '/post/',
+        type : 'POST',
+        data : { msgbox : $('#chat-msg').val() },
+
+        success : function(json){
+            $('#chat-msg').val('');
+            //$('#msg-list').append('<li class="text-left query list-group-item">' + json.query + '</li>');
+
+            //$('#msg-list').append('<li class="text-left response list-group-item">' + json.response + '</li>');
+            $('#msg-list').append('<div class="row msg_container base_sent">'
+                                    +'<div class="col-md-10 col-xs-10">'
+                                        +'<div class="messages msg_sent" style="text-align: right;">'
+                                           +'<p class="query">' + json.query + '</p>'
+                                        +'</div></div></div>');
+
+            $('#msg-list').append('<div class="row msg_container base_receive">'
+                                    +'<div class="col-md-2 col-xs-2 avatar">'
+                                        +' <img src="http://127.0.0.1:8090/static/img/profile-pic.png" class=" img-responsive ">'
+                                    +'</div>'
+                                    +'<div class="col-md-10 col-xs-10">'
+                                        +'<div class="messages msg_receive">'
+                                           +'<p class="response">' + json.response + '</p>'
+                                        +'</div></div></div>');
+
+            var chatlist = document.getElementById('msg-list-div');
+            chatlist.scrollTop = chatlist.scrollHeight;
+        }
+    });
+});
+
+
+
+var scrolling = false;
+$(function(){
+    $('#msg-list-div').on('scroll', function(){
+        scrolling = true;
+    });
+    refreshTimer = setInterval(getMessages, 500);
+});
+
+$(document).ready(function() {
+     $('#send').attr('disabled','disabled');
+     $('#chat-msg').keyup(function() {
+        if($(this).val() != '') {
+           $('#send').removeAttr('disabled');
+        }
+        else {
+        $('#send').attr('disabled','disabled');
+        }
+     });
+ });
+
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
